de51754af4179afb8d9d1f4593f6af9e54359ee3
shock59 <sam@bladeinteractive.com.au>
2022-05-15 17:01:40 +1000

    indentation

--- commands/timein.js
+++ commands/timein.js
@@ -2,31 +2,30 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import moment from "moment-timezone";
 
 const info = {
-    data: new SlashCommandBuilder()
-        .setDescription(
-            "Get the time in a scpecific timezone",
-        )
-        .addStringOption(option =>
-		option.setName('timezone')
-			.setDescription('Your timezone')
-			.setRequired(true)),
+  data: new SlashCommandBuilder()
+    .setDescription(
+      "Get the time in a scpecific timezone",
+    )
+    .addStringOption(option =>
+      option.setName('timezone')
+        .setDescription('Your timezone')
+        .setRequired(true)),
 
-    async interaction(interaction) {
-        const timezone = interaction.options.getString("timezone");
-        if (timezone == "+" || timezone == "-") {
-            console.log(".")
-        };
-        if (moment.tz.zone(timezone) != null) {
-            const timezoneName = ((moment.tz(timezone).format("zz")[0].toLowerCase() != moment.tz(timezone).format("zz")[0].toUpperCase()) 
-                                   ? `${ moment.tz(timezone).format("zz") } (UTC${ moment.tz(timezone).format("Z") })`
-                                   : `UTC${ moment.tz(timezone).format("Z") }`)
+  async interaction(interaction) {
+      const timezone = interaction.options.getString("timezone");
+      if (timezone == "+" || timezone == "-") {
+        
+      };
+      if (moment.tz.zone(timezone) != null) {
+        const timezoneName = ((moment.tz(timezone).format("zz")[0].toLowerCase() != moment.tz(timezone).format("zz")[0].toUpperCase()) 
+                              ? `${ moment.tz(timezone).format("zz") } (UTC${ moment.tz(timezone).format("Z") })`
+                              : `UTC${ moment.tz(timezone).format("Z") }`)
 
-            await interaction.reply(`The time in ${ timezoneName } is **${ moment.tz(timezone).format("HH:mm") }**`);
-            return
-        } else {
-            await interaction.reply("Invalid zone!");
-        }
-    }
+        await interaction.reply(`The time in ${ timezoneName } is **${ moment.tz(timezone).format("HH:mm") }**`);
+      } else {
+        await interaction.reply("Invalid zone!");
+      }
+  }
 };
 
 export default info;
